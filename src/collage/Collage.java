3896c329c9728bbb02bf1dcaa1b868754c5f926c
Mahmoud-kamal12 <mahmoud192551@fci.bu.edu.eg>
2020-12-24 18:34:15 +0200

    Creat

---
+++ src/collage/Collage.java
@@ -0,0 +1,15 @@
+
+package collage;
+
+import Classes.*;
+import GUI.*;
+import ourFun.*;
+
+public class Collage {
+
+    public static void main(String[] args) {
+       Main f = new Main();
+       f.setVisible(true);
+    }
+    
+}
